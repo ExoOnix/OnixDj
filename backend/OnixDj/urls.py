c22c565f7143dbb5c6ef4997d83a927335079ee9
ExoticOnix <mshapirovskyy@gmail.com>
2025-02-27 20:07:09 +0200

    orginized api

--- backend/OnixDj/urls.py
+++ backend/OnixDj/urls.py
@@ -21,8 +21,5 @@ from users.views import custom_confirm_email_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^dj-rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', custom_confirm_email_view, name='account_confirm_email'),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent')
+    path('api/', include('users.urls')),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 import requests
 
 def custom_confirm_email_view(request, key):
-    base_url = request.build_absolute_uri('/')[:-1]
+    base_url = request.build_absolute_uri('/api/')[:-1]
     api_url = f"{base_url}/dj-rest-auth/registration/verify-email/"
 
     response = requests.post(api_url, json={"key": key})
