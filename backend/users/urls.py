a5affd496424a67bf34226d693dfeb7d57a7bc2e
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-01 22:45:22 +0200

    removed useless urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from dj_rest_auth.registration.views import VerifyEmailView
 from users.views import custom_confirm_email_view
 
 urlpatterns = [
     re_path(r'^dj-rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', custom_confirm_email_view, name='account_confirm_email'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent')
 ]
