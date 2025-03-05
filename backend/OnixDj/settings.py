69715e59ae7cddd386c82a68a4c65fd3e8667af6
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-05 15:25:29 +0200

    email confirm url change

--- backend/OnixDj/settings.py
+++ backend/OnixDj/settings.py
@@ -64,7 +64,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 EMAIL_CONFIRM_REDIRECT_BASE_URL = \
-    "http://localhost:3000/email/confirm/"
+    "/email/confirm/"
 
 # EMAILS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
