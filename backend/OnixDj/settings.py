e4d432798a3ddea21ca900dd0730aeecc19eb9f8
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-05 15:23:07 +0200

    added mailhog support to docker dev env

--- backend/OnixDj/settings.py
+++ backend/OnixDj/settings.py
@@ -68,11 +68,11 @@ EMAIL_CONFIRM_REDIRECT_BASE_URL = \
 
 # EMAILS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = os.getenv("EMAIL_HOST" ,'localhost')
+EMAIL_PORT = int(os.getenv("EMAIL_PORT" ,1025))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER" ,'')
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD" ,'')
 
 # Application definition
 
