61c96f92b1e8af3ebb1b16f67b606a75fef47d7f
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-05 15:01:04 +0200

    dev env

--- backend/OnixDj/settings.py
+++ backend/OnixDj/settings.py
@@ -151,9 +151,9 @@ else:
             'ENGINE': 'django.db.backends.{}'.format(
                 os.getenv('DATABASE_ENGINE', 'sqlite3')
             ),
-            'NAME': os.getenv('DATABASE_NAME', 'polls'),
-            'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+            'NAME': os.getenv('POSTGRES_DB', 'polls'),
+            'USER': os.getenv('POSTGRES_USER', 'myprojectuser'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
             'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
             'PORT': os.getenv('DATABASE_PORT', 5432),
         }
