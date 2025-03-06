f35aac68051150ea395de1840bbe4fea3cbc0838
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-06 22:22:24 +0200

    fixed redefinition of unused settings

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,6 @@ from dj_rest_auth.registration.views import VerifyEmailView
 from dj_rest_auth.utils import jwt_encode
 from rest_framework.response import Response
 from rest_framework import status
-from django.conf import settings
 from django.contrib.auth import login as django_login
 
 
