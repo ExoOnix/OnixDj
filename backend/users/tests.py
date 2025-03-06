40724c5d223f2dee38597a2c103e637741a04493
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-06 21:42:40 +0200

    test

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -29,7 +29,7 @@ class UsersManagersTests(TestCase):
     def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
-            email="super@user.com", password="foo"
+            email="super@user.com", password="foobar"
         )
         self.assertEqual(admin_user.email, "super@user.com")
         self.assertTrue(admin_user.is_active)
@@ -43,5 +43,5 @@ class UsersManagersTests(TestCase):
             pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
-                email="super@user.com", password="foo", is_superuser=False
+                email="super@user.com", password="foobar", is_superuser=False
             )
