53c5db9afd94d489fe2be61822652da98b71e92e
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-06 22:20:50 +0200

    on confirm email, login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include, re_path
 from users.views import custom_confirm_email_view
+from users.views import CustomVerifyEmailView
 
 urlpatterns = [
     re_path(
         r"^dj-rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$",
         custom_confirm_email_view,
         name="account_confirm_email",
+    ),
+    path(
+        "dj-rest-auth/registration/verify-email/",
+        CustomVerifyEmailView.as_view(),
+        name="custom_verify_email",
     ),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path(
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,44 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from dj_rest_auth.registration.views import VerifyEmailView
+from dj_rest_auth.utils import jwt_encode
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from django.contrib.auth import login as django_login
 
 
 def custom_confirm_email_view(request, key):
     return HttpResponseRedirect(
         f"{settings.EMAIL_CONFIRM_REDIRECT_BASE_URL}{key}/"
     )
+
+
+class CustomVerifyEmailView(VerifyEmailView):
+    def post(self, request, *args, **kwargs):
+        # Deserialize the incoming data
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.kwargs["key"] = serializer.validated_data["key"]
+
+        # Retrieve the confirmation object
+        confirmation = self.get_object()
+        confirmation.confirm(self.request)
+
+        # Generate JWT tokens for the user
+        user = confirmation.email_address.user
+
+        django_login(request, user)
+
+        access_token, refresh_token = jwt_encode(user)
+
+        # Prepare the response data
+        response_data = {
+            "user": str(user),
+            "access": str(access_token),
+            "refresh": str(refresh_token),
+        }
+
+        # Optionally, set the refresh token as an HTTP-only cookie
+        response = Response(response_data, status=status.HTTP_200_OK)
+        return response
