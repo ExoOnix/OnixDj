276844f2d2d7ffe6de20427b1c31c51631df898b
ExoticOnix <mshapirovskyy@gmail.com>
2025-03-08 10:47:12 +0200

    test

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include, re_path
 from users.views import custom_confirm_email_view
 from users.views import CustomVerifyEmailView
+from users.views import hello_view
 
 urlpatterns = [
     re_path(
@@ -17,4 +18,5 @@ urlpatterns = [
     path(
         "dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
     ),
+    path("helloview/", hello_view),
 ]
